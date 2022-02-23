89efdde91d4f845fa6a3f8cb385492d8d94bd3ec
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-02-23 06:47:43 +0530

    Add files via upload

---
+++ AdapterExample.java
@@ -0,0 +1,21 @@
+
+import java.awt.*;    
+import java.awt.event.*;    
+
+public class AdapterExample {  
+	Frame f;   
+	AdapterExample() {    
+		f = new Frame ("Window Adapter");   
+		f.addWindowListener (new WindowAdapter() {    
+			public void windowClosing (WindowEvent e) {    
+				f.dispose();    
+			}    
+		});    
+		f.setSize (400, 400);    
+		f.setLayout (null);    
+		f.setVisible (true);    
+	}    
+	public static void main(String[] args) {    
+		new AdapterExample();
+	} 
+}
