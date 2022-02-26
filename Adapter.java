2b053f0e8619c764ea26ac067fba0348db0c35a5
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-02-26 12:31:33 +0530

    Update Adapter.java

--- Adapter.java
+++ Adapter.java
@@ -1,21 +1,29 @@
-
-import java.awt.*;    
-import java.awt.event.*;    
-
-public class Adapter {  
-	Frame f;   
-	Adapter() {    
-		f = new Frame ("Window Adapter");   
-		f.addWindowListener (new WindowAdapter() {    
-			public void windowClosing (WindowEvent e) {    
-				f.dispose();    
-			}    
-		});    
-		f.setSize (400, 400);    
-		f.setLayout (null);    
-		f.setVisible (true);    
-	}    
-	public static void main(String[] args) {    
-		new Adapter();
-	} 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;  
+public class keyexample extends KeyAdapter{  
+	public static void main(String[] args) {  
+		JFrame f = new JFrame("Keyboard");
+		f.setSize(300,300);  
+		keyexample k = new keyexample();
+		f.addKeyListener(k);
+		JPanel p = new JPanel();
+		l = new JLabel();
+		p.add(l);
+		f.add(p);
+		f.setVisible(true); 
+	}  	
+	static JLabel l; 
+	public void keyPressed(KeyEvent e) {  
+		l.setText("key is pressed");  
+	}  
+	public void keyReleased(KeyEvent e) {  
+		l.setText("Key is released");  
+	}  
+	public void keyTyped(KeyEvent e) {  
+		l.setText("Typing");  
+	}  
+	
 }
+
+
