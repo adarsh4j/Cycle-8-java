807a63a8e7e9a769af637b27512f1035927de5df
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-02-27 11:48:45 +0530

    Update Draw.java

--- Draw.java
+++ Draw.java
@@ -5,31 +5,31 @@ import javax.swing.*;
 public class Draw extends JPanel
 
 {
-
-  public void paint(Graphics g){
-
-    g.drawLine(20,20,200,180);
-
-    g.drawRect(50, 35, 150, 150);  
-
-    g.drawOval(50,40,150,150);
-
- }
-
-  
-
-  public static void main(String[] args){
-
-    JFrame f = new JFrame("Draw figures");
-
-    f.getContentPane().add(new Draw());
-
-    f.setSize(250, 250);
-
-    f.setVisible(true);
-
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-  }
-
+	
+	public void paint(Graphics g){
+		
+		g.drawLine(20,10,50,44);
+		
+		g.drawRect(100, 80, 15, 20);  
+		
+		g.drawOval(50,60,40,32);
+		
+	}
+	
+	
+	
+	public static void main(String[] args){
+		
+		JFrame f = new JFrame("Draw figures");
+		
+		f.getContentPane().add(new Draw());
+		
+		f.setSize(250, 250);
+		
+		f.setVisible(true);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
 }
